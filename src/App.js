2ae98a50b553d965bb261fc41016e6adfd3f1db8
Vilmokas <42962270+Vilmokas@users.noreply.github.com>
2021-11-19 08:17:02 +0200

    feat: change add new task and button styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Container, Row, Col, ListGroup, Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [toDoList, setToDoList] = useState([]);
@@ -29,22 +31,79 @@ function App() {
   };
 
   return (
-    <div>
-      {toDoList.map((task, index) => (
-        <p key={index}>
-          task: {task.task} | status: {task.status.toString()}
-          <button onClick={() => onTaskDone(index)}>✔️</button>
-          <button onClick={() => onTaskRemove(index)}>❌</button>
-        </p>
-      ))}
-      <form onSubmit={handleSubmit}>
-        <label>
-          New Task:
-          <input type="text" value={newTask} onChange={handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col lg={8}>
+          <ListGroup>
+            {toDoList.map((task, index) => (
+              <ListGroup.Item key={index}>
+                <Row>
+                  <Col md={8}>
+                    task: {task.task} | status: {task.status.toString()}
+                  </Col>
+                  <Col md={{ span: 3, offset: 1 }}>
+                    <Button
+                      variant="outline-success"
+                      onClick={() => onTaskDone(index)}
+                    >
+                      ✔️
+                    </Button>{" "}
+                    <Button
+                      variant="outline-danger"
+                      onClick={() => onTaskRemove(index)}
+                    >
+                      ❌
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+          {/* <Row className="justify-content-md-center">
+            <Col md={{ span: 7, offset: 1 }}> */}
+          {/* <form onSubmit={handleSubmit}>
+                <label>
+                  New Task:
+                  <input type="text" value={newTask} onChange={handleChange} />
+                </label>{" "}
+                <Button variant="outline-success" type="submit" value="Submit">
+                  💾
+                </Button>
+              </form> */}
+          <Form onSubmit={handleSubmit}>
+            <Row>
+              <Col md={12}>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlTextarea1"
+                >
+                  <Form.Label>New Task:</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={2}
+                    value={newTask}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+              </Col>
+              <Col md={{ span: 4, offset: 4 }}>
+                <Row>
+                  <Button
+                    variant="outline-success"
+                    type="submit"
+                    value="Submit"
+                  >
+                    💾
+                  </Button>
+                </Row>
+              </Col>
+            </Row>
+          </Form>
+          {/* </Col>
+          </Row> */}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
